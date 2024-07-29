c993aa6c0dff6a05937d211b02b1a442a027dc53
alicansadeler <alicansadeler@gmail.com>
2024-07-29 14:21:48 +0300

    cuboid paketi görevleri tamamlandi

---
+++ src/main/java/com/workintech/pool/Cuboid.java
@@ -0,0 +1,24 @@
+package com.workintech.pool;
+
+public class Cuboid extends Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        setHeight(height);
+    }
+
+
+    public void setHeight(double height) {
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+
+    public double getVolume() {
+        return  super.getArea() * this.height;
+    }
+}
